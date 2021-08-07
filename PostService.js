77146746cd20984886186eef6907889ca4f8165f
Kosteyyy <averkiev@yandex.ru>
2021-08-07 23:10:12 +0300

    PostService full

--- PostService.js
+++ PostService.js
@@ -6,52 +6,32 @@ class PostService {
         return createdPost;
     }
 
-    async getAll(req, res) {
-        try {
-            const posts = await Post.find();
-            return res.json(posts);
-        } catch (e) {
-            res.status(500).json(e)
-        }
+    async getAll() {
+        const posts = await Post.find();
+        return posts;
     }
 
-    async getOne(req, res) {
-        try {
-            const {id} = req.params;
-            if (!id) {
-                res.status(400).json({message: 'Id не указан'});
+    async getOne(id) {
+        if (!id) {
+               throw new Error('Id не указан');
             }
-            const post = await Post.findById(id);
-            return res.json(post);
-        } catch (e) {
-            res.status(500).json(e)
-        }       
+        const post = await Post.findById(id);
+        return post;
     }
-    async update(req, res) {
-        try {
-            const post = req.body;
-            if (!post._id) {
-                res.status(400).json({message: 'Id не указан'});
-            }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true});
-            return res.json(updatedPost);
-        } catch (e) {
-            res.status(500).json(e)
-        }        
+    async update(post) {
+        if (!post._id) {
+            throw new Error('Id не указан');
+        }
+        const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true});
+        return updatedPost;
     }
-    async delete(req, res) {
-        try {
-            const {id} = req.params;
+    async delete(id) {
             if (!id) {
-                res.status(400).json({message: 'Id не указан'});
+               throw new Error('Id не указан');
             }
             const post = await Post.findByIdAndDelete(id);
-            return res.json(post);
-        } catch (e) {
-            res.status(500).json(e)
-        }        
+            return post;
     }
-
 }
 
 export default new PostService();
