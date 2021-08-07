30b19a5bd45d2184983b796e7f661a7761fe5baa
Kosteyyy <averkiev@yandex.ru>
2021-08-07 15:44:05 +0300

    PostController create

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import {DB_URL} from './mongoaccess.js';
-import Post from "./Post.js";
+import router from "./router.js";
 
 const PORT = 5000;
 
 const app = express()
 
 app.use(express.json())
-
-// app.get('/', (req, res) => {
-//     console.log(req.query.test); //http://localhost:5000/?test=here&quary=something&page=Homepage
-//     res.status(200).json('Server working on port ')
-// })
-
-app.post('/', async (req, res) => {
-    try {
-        const {author, title, content, picture} = req.body;
-        const post = await Post.create({author, title, content, picture});
-        // console.log(req.body); 
-        res.json(post)
-    } catch (e) {
-        res.status(500).json(e)
-    }
-})
+app.use('/api', router)
 
 async function startApp() {
     try {
